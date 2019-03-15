cf49bd0138652fa4a68753b00d345997e314c3f8
mrkraimer <mrkraimer@comcast.net>
2019-03-15 13:30:48 -0400

    client for table

--- src/tableClient.cpp
+++ src/tableClient.cpp
@@ -8,61 +8,266 @@
  */
 
 /* Author: Marty Kraimer */
-
 #include <iostream>
-
+#include <epicsGetopt.h>
 #include <pv/pvaClient.h>
+#include <pv/convert.h>
 
+using std::tr1::static_pointer_cast;
 using namespace std;
 using namespace epics::pvData;
 using namespace epics::pvAccess;
 using namespace epics::pvaClient;
 
+class ClientPut;
+typedef std::tr1::shared_ptr<ClientPut> ClientPutPtr;
+
+class ClientPut :
+    public PvaClientChannelStateChangeRequester,
+    public PvaClientPutRequester,
+    public std::tr1::enable_shared_from_this<ClientPut>
+{
+private:
+    string channelName;
+    string providerName;
+    string request;
+    bool channelConnected;
+    bool putConnected;
+
+    PvaClientChannelPtr pvaClientChannel;
+    PvaClientPutPtr pvaClientPut;
+
+    void init(PvaClientPtr const &pvaClient)
+    {
+        pvaClientChannel = pvaClient->createChannel(channelName,providerName);
+        pvaClientChannel->setStateChangeRequester(shared_from_this());
+        pvaClientChannel->issueConnect();
+    }
+public:
+    POINTER_DEFINITIONS(ClientPut);
+    ClientPut(
+        const string &channelName,
+        const string &providerName,
+        const string &request)
+    : channelName(channelName),
+      providerName(providerName),
+      request(request),
+      channelConnected(false),
+      putConnected(false)
+    {
+    }
+    
+    static ClientPutPtr create(
+        PvaClientPtr const &pvaClient,
+        const string & channelName,
+        const string & providerName,
+        const string  & request)
+    {
+        ClientPutPtr client(ClientPutPtr(
+             new ClientPut(channelName,providerName,request)));
+        client->init(pvaClient);
+        return client;
+    }
+
+    virtual void channelStateChange(PvaClientChannelPtr const & channel, bool isConnected)
+    {
+        channelConnected = isConnected;
+        if(isConnected) {
+            if(!pvaClientPut) {
+                pvaClientPut = pvaClientChannel->createPut(request);
+                pvaClientPut->setRequester(shared_from_this());
+                pvaClientPut->issueConnect();
+            }
+        }
+    }
+
+    virtual void channelPutConnect(
+        const epics::pvData::Status& status,
+        PvaClientPutPtr const & clientPut)
+    {
+        putConnected = true;
+        cout << "channelPutConnect " << channelName << " status " << status << endl;
+    }
+
+    
+    virtual void putDone(
+        const epics::pvData::Status& status,
+        PvaClientPutPtr const & clientPut)
+    {
+         cout << "putDone " << channelName << " status " << status << endl;
+    }
+
+    
+    virtual void getDone(
+        const epics::pvData::Status& status,
+        PvaClientPutPtr const & clientPut)
+    {
+         cout << "getDone " << channelName << " status " << status << endl;
+          if(status.isOK()) {
+             cout << pvaClientPut->getData()->getPVStructure() << endl;
+         } else {
+             cout << "getGetDone " << channelName << " status " << status << endl;
+         }
+    }
+
+
+    void put()
+    {
+        if(!channelConnected) {
+            cout << channelName << " channel not connected\n";
+            return;
+        }
+        if(!putConnected) {
+            cout << channelName << " channelPut not connected\n";
+            return;
+        }
+        PvaClientPutDataPtr putData = pvaClientPut->getData();
+        PVStructurePtr pvStructure = putData->getPVStructure()->getSubField<PVStructure>("value");
+        if(!pvStructure) {
+            cout << "value is not a structure\n";
+            return;
+        }
+        PVFieldPtrArray pvFields = pvStructure->getPVFields();
+        ConvertPtr convert = getConvert();
+        for(size_t ind = 0; ind<pvFields.size(); ++ind)
+        {
+            PVFieldPtr pvField = pvFields[ind];
+            cout << "starting field " << pvField->getFieldName() << "\n";
+            if(pvField->getField()->getType()!=scalarArray) {
+                 cout << "fields is not a scalar array\n";
+                 continue;
+            }
+            PVScalarArrayPtr pvScalarArray = static_pointer_cast<PVScalarArray>(pvField);
+            cout << "enter value or values to put\n";
+            string value;
+            getline(cin,value);
+            vector<string> values;
+            size_t pos = 0;
+            size_t n = 1;
+            while(true)
+            {
+                size_t offset = value.find(" ",pos);
+                if(offset==string::npos) {
+                    values.push_back(value.substr(pos));
+                    break;
+                }
+                values.push_back(value.substr(pos,offset-pos));
+                pos = offset+1;
+                n++;    
+            }
+            pvScalarArray->setLength(n);
+            convert->fromStringArray(pvScalarArray,0,n,values,0);        
+        }
+        pvaClientPut->issuePut();
+    }
+
+    void get()
+    {
+        if(!channelConnected) {
+            cout << channelName << " channel not connected\n";
+            return;
+        }
+        if(!putConnected) {
+            cout << channelName << " channelPut not connected\n";
+            return;
+        }
+        pvaClientPut->issueGet();
+    }
+};
+
+
 int main(int argc,char *argv[])
 {
-    PvaClientPtr pva = PvaClient::get("pva");
-    try {
-        PvaClientChannelPtr pvaChannel = pva->channel("powerSupply"); 
-        PvaClientPutGetPtr putGet(pvaChannel->createPutGet(
-                "putField(power.value,voltage.value)getField()"));
-        putGet->connect();
-        PvaClientPutDataPtr putData(putGet->getPutData());
-        PvaClientGetDataPtr getData(putGet->getGetData());
-        PVStructurePtr pvStructure = putData->getPVStructure();
-        pvStructure = putData->getPVStructure();
-        PVDoublePtr putPower = pvStructure->getSubField<PVDouble>("power.value");
-        PVDoublePtr putVoltage = pvStructure->getSubField<PVDouble>("voltage.value");
-        putPower->put(5.0);
-        putVoltage->put(5.0);
-        putGet->putGet();
-        
-        pvStructure = getData->getPVStructure();
-        PVDoublePtr gotPower = pvStructure->getSubField<PVDouble>("power.value");
-        PVDoublePtr gotVoltage = pvStructure->getSubField<PVDouble>("voltage.value"); 
-        PVDoublePtr gotCurrent = pvStructure->getSubField<PVDouble>("current.value");
-        if(gotPower->get() == 5.0) cout <<  "returned correct power\n";
-        if(gotVoltage->get() == 5.0) cout <<  "returned correct voltage\n";
-        if(gotCurrent->get() == 1.0) cout <<  "returned correct current\n";
+    string provider("pva");
+    string channelName("table");
+    string request("value");
+    string debugString;
+    bool debug(false);
+    int opt;
+    while((opt = getopt(argc, argv, "hp:r:d:")) != -1) {
+        switch(opt) {
+            case 'p':
+                provider = optarg;
+                break;
+            case 'r':
+                request = optarg;
+                break;
+            case 'h':
+             cout << " -h -p provider -r request - d debug channelNames " << endl;
+             cout << "default" << endl;
+             cout << "-p " << provider 
+                  << " -r " << request
+                  << " -d " << (debug ? "true" : "false") 
+                  << " " <<  channelName
+                  << endl;           
+                return 0;
+            case 'd' :
+               debugString =  optarg;
+               if(debugString=="true") debug = true;
+               break;
+            default:
+                std::cerr<<"Unknown argument: "<<opt<<"\n";
+                return -1;
+        }
+    }
+    bool pvaSrv(((provider.find("pva")==string::npos) ? false : true));
+    bool caSrv(((provider.find("ca")==string::npos) ? false : true));
+    if(pvaSrv&&caSrv) {
+        cerr<< "multiple providers are not allowed\n";
+        return 1;
+    }
+    cout << "provider " << provider
+         << " channelName " <<  channelName
+         << " request " << request
+         << " debug " << (debug ? "true" : "false")  << endl;
+
+    cout << "_____put starting__\n";
     
-        putPower->put(10.0);
-        putGet->putGet();
-    
-        pvStructure = getData->getPVStructure();
-        gotPower = pvStructure->getSubField<PVDouble>("power.value");
-        gotVoltage = pvStructure->getSubField<PVDouble>("voltage.value");
-        gotCurrent = pvStructure->getSubField<PVDouble>("current.value");
-        if(gotPower->get() == 10.0) cout << "returned correct power\n"; 
-        if(gotVoltage->get() == 5.0) cout << "returned correct voltage\n";
-        if(gotCurrent->get() == 2.0) cout << "returned correct current\n";
-        
-        putPower->put(5.0);
-        putVoltage->put(0.0);
-        cout << "NOTE!!! an exception will be thrown because voltage is 0\n";
-        putGet->putGet();
+    try {   
+        if(debug) PvaClient::setDebug(true);
+        vector<string> channelNames;
+        vector<ClientPutPtr> ClientPuts;
+        int nPvs = argc - optind;       /* Remaining arg list are PV names */
+        if (nPvs==0)
+        {
+            channelNames.push_back(channelName);
+            nPvs = 1;
+        } else {
+            for (int n = 0; optind < argc; n++, optind++) channelNames.push_back(argv[optind]);
+        }
+        PvaClientPtr pva= PvaClient::get(provider);
+        for(int i=0; i<nPvs; ++i) {
+            ClientPuts.push_back(ClientPut::create(pva,channelNames[i],provider,request));
+        }
+        while(true) {
+            cout << "enter one of: exit put get\n";
+            int c = std::cin.peek();  // peek character
+            if ( c == EOF ) continue;
+            string str;
+            getline(cin,str);
+            if(str.compare("exit")==0) break;
+            if(str.compare("get")==0) {
+                 for(int i=0; i<nPvs; ++i) {
+                    try {
+                         ClientPuts[i]->get();
+                    } catch (std::exception& e) {
+                       cerr << "exception " << e.what() << endl;
+                    }
+                 }
+                 continue;
+            }
+            if(str.compare("put")!=0) continue;
+            for(int i=0; i<nPvs; ++i) {
+                try {
+                    ClientPuts[i]->put();
+                } catch (std::exception& e) {
+                   cerr << "exception " << e.what() << endl;
+                }
+            }
+        }
     } catch (std::exception& e) {
-        cout << "exception " << e.what() << endl;
-        exit(1);
+        cerr << "exception " << e.what() << endl;
+        return 1;
     }
     return 0;
 }
-
